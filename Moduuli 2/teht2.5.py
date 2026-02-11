c4c38675ee3a028d4d79eb4d7572e513eff56819
SiiriJok <siiri.jokinen2@metropolia.fi>
2026-02-11 09:31:20 +0200

    Moduuli 2

---
+++ Moduuli 2/teht2.5.py
@@ -0,0 +1,9 @@
+leiviskat = float(input("Anna leiviskat: "))
+naulat = float(input("Anna naulat: "))
+luodit = float(input("Anna luodit: "))
+
+luoti_grammoina = 13.3
+naula_grammoina = 32 * luoti_grammoina
+leiviska_grammoina = 20 * naula_grammoina
+
+massa_grammoina = (leiviskat * leiviska_grammoina + naulat)
