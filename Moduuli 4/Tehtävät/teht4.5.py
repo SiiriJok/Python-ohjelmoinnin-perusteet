089eb269e0a31e828850da016074115c4c3023e3
SiiriJok <siiri.jokinen2@metropolia.fi>
2026-02-25 14:42:31 +0200

    Moduuli 4

--- Moduuli 4/Tehtävät/teht4.5.py
+++ Moduuli 4/Tehtävät/teht4.5.py
@@ -1,10 +1,24 @@
-#Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja salasanan. Jos jompikumpi tai molemmat ovat
-#väärin, tunnus ja salasana kysytään uudelleen. Tätä jatketaan kunnes kirjautumistiedot ovat
+#Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja salasanan. Jos jompikumpi tai molemmat
+#ovat väärin, tunnus ja salasana kysytään uudelleen. Tätä jatketaan kunnes kirjautumistiedot ovat
 #oikein tai väärät tiedot on syötetty viisi kertaa. Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
 
 tunnus = input("Käyttäjätunnus: ")
 salasana = input("Salasana: ")
 
-kerta = 5
+oikea_t = "python"
+oikea_s = "rules"
 
-while
+arvaus = 1
+
+while arvaus <= 4:
+    if tunnus == oikea_t and salasana == oikea_s:
+        print("Tervetuloa!")
+        break
+    elif tunnus != oikea_t or salasana != oikea_s:
+        print("Salasana tai tunnus on väärin, yritä uudelleen!")
+        arvaus += 1
+        tunnus = input("Käyttäjätunnus: ")
+        salasana = input("Salasana: ")
+
+else:
+    print("Pääsy evätty")
