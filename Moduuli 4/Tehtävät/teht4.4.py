14a05b9fa51ab714bca259fa28d8bef39f8dfdae
SiiriJok <siiri.jokinen2@metropolia.fi>
2026-02-25 12:58:47 +0200

    Moduuli 4

--- Moduuli 4/Tehtävät/teht4.4.py
+++ Moduuli 4/Tehtävät/teht4.4.py
@@ -1,4 +1,22 @@
+#Kirjoita peli, jossa tietokone arpoo kokonaisluvun väliltä 1..10. Kone arvuuttelee lukua pelaajalta siihen asti,
+#kunnes tämä arvaa oikein. Kunkin arvauksen jälkeen ohjelma tulostaa tekstin Liian suuri arvaus,
+#Liian pieni arvaus tai Oikein. Huomaa, että tietokone ei saa vaihtaa lukuaan arvauskertojen välissä.
+
 import random
 
-luku = random.randint(1,10)
+oikea = random.randint(1,10)
+
+while True:
+    luku = int(input("Arvaa luku 1 ja 10 väliltä: "))
+    if luku == oikea:
+        break
+    elif luku < oikea:
+        print("Liian pieni arvaus")
+    elif luku > oikea:
+        print("Liian suuri arvaus")
+
+else:
+    print("Luvun pitää olla 1 ja 10 väliltä")
+
+print("Oikein!")
 
