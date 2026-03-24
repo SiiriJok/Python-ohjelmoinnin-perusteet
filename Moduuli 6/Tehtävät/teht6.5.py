0a64caf716fdb057819b4167f050177ce161d1ea
SiiriJok <siiri.jokinen2@metropolia.fi>
2026-03-24 13:52:54 +0200

    Viikko 6

--- Moduuli 6/Tehtävät/teht6.5.py
+++ Moduuli 6/Tehtävät/teht6.5.py
@@ -5,6 +5,20 @@ testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat s
 karsitun listan.
 """
 
+def karsi_parittomat(lista):
+    uusi_lista = []
+
+    for luku in lista:
+        if luku % 2 == 0:
+            uusi_lista.append(luku)
+
+    return uusi_lista
 
 
 luvut = [1,2,3,4,5,6,7,8,9,10]
+
+karsittu = karsi_parittomat(luvut)
+
+print("Alkuperäinen lista: ",luvut)
+print("Karsittu lista: ",karsittu)
+
--- Moduuli 6/Tehtävät/teht6.6.py
+++ Moduuli 6/Tehtävät/teht6.6.py
@@ -4,3 +4,32 @@ Funktio laskee ja palauttaa pizzan yksikköhinnan euroina per neliömetri. Pää
 halkaisijat ja hinnat sekä ilmoittaa, kumpi pizza antaa paremman vastineen rahalle (eli kummalla on alhaisempi
 yksikköhinta). Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
 """
+
+import math
+
+def yksikkohinta(halkaisija, hinta):
+    halkaisija_m = halkaisija / 100
+    sade = halkaisija_m / 2
+    pinta_ala = math.pi * sade ** 2
+    hinta_per_neliometri = hinta / pinta_ala
+    return hinta_per_neliometri
+
+
+halkaisija1 = float(input("Anna ensimmäisen pizzan halkaisija senttimetreinä: "))
+hinta1 = float(input("Anna ensimmäisen pizzan hinta euroina: "))
+
+halkaisija2 = float(input("Anna toisen pizzan halkaisija senttimetreinä: "))
+hinta2 = float(input("Anna toisen pizzan hinta euroina: "))
+
+yksikkohinta1 = yksikkohinta(halkaisija1, hinta1)
+yksikkohinta2 = yksikkohinta(halkaisija2, hinta2)
+
+print(f"Ensimmäisen pizzan yksikköhinta on {yksikkohinta1:.2f} euroa per neliömetri.")
+print(f"Toisen pizzan yksikköhinta on {yksikkohinta2:.2f} euroa per neliömetri.")
+
+if yksikkohinta1 < yksikkohinta2:
+    print("Ensimmäinen pizza antaa paremman vastineen rahalle.")
+elif yksikkohinta2 < yksikkohinta1:
+    print("Toinen pizza antaa paremman vastineen rahalle.")
+else:
+    print("Pizzat ovat samanarvoisia.")
